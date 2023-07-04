c708392648b41817071dc3c3f225faa186748ccd
Dadaskis <dadaskis@gmail.com>
2023-07-04 23:35:51 +0300

    Changing git_lazy_push.py

---
+++ git_lazy_push.py
@@ -0,0 +1,54 @@
+from subprocess import Popen, PIPE
+from codecs import decode
+
+while True:
+	print("//")
+	print("Press enter to pull")
+	print("Enter anything for commit message")
+	print("::")
+	msg = input()
+
+	if msg == "":
+		print("Pulling.")
+		Popen(
+			[
+				'git', 'pull'
+			]
+		).wait()
+		continue
+
+	print("Pushing.")
+
+	Popen(
+		[
+			'git', 'add', '*'
+		]
+	).wait()
+
+	Popen(
+		[
+			'git', 'add', '-u'
+		]
+	).wait()
+
+	Popen(
+		[
+			'git', 'commit', '-m', msg
+		]
+	).wait()
+
+	Popen(
+		[
+			'git', 'log', '--max-count', '1'
+		]
+	).wait()
+
+	Popen(
+		[
+			"git", "push", "origin", "master"
+		]
+	)
+
+	print()
+	print()
+	print()
