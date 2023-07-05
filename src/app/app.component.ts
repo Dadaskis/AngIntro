69326ac5fb71e8a4eb300cdef3bdd73c3d3d2ffe
Dadaskis <dadaskis@gmail.com>
2023-07-05 20:04:14 +0300

    Fixing form-fill replacement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -67,9 +67,8 @@ export class AppComponent implements AfterViewInit {
     if (body == null) {
       return
     }
-    let text = document.createElement("form-fill")
-    text.innerText = "Placeholder"
+    let formFill = document.createElement("form-fill")
     appRoot.remove()
-    body.append(text)
+    body.append(formFill)
   }
 }
