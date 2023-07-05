883c92a538b65b67b714cb1c029351aa97d08741
root <root@Linux.myguest.virtualbox.org>
2023-07-05 14:52:15 +0300

    Generate component: Test2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostComponent } from './post/post.component';
-import { TestComponent } from './test/test.component'
+import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     PostComponent,
-    TestComponent
+    TestComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule,
