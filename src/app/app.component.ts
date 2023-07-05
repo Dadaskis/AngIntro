4f94a27ab2adf88ab867d46a8cd6bb85a4c2a250
Dadaskis <dadaskis@gmail.com>
2023-07-05 15:45:12 +0300

    Trying to fix "any type" error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   title = 'AngIntro';
   parentMessage: string = "Parent Message";
   
-  @ViewChild(PostComponent) childComponent;
+  @ViewChild(PostComponent) childComponent: PostComponent;
 
   constructor() {
     console.log(this.childComponent.childMessage);
