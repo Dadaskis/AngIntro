1534905e7652637a1b68ac9d6ee9dab6d060f2a3
Dadaskis <dadaskis@gmail.com>
2023-07-05 14:28:04 +0300

    Changing color for navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
 	selector: 'app-navbar',
-	template: '<h1>Navbar Component</h1>'
+	template: '<h1>Navbar Component</h1>',
+	styles: [
+		'h1 { color: red }'
+	]
 })
 export class NavbarComponent {
 
