d11c4f755d7c01817232741eb6afb7feb66b6457
Dadaskis <dadaskis@gmail.com>
2023-07-05 15:48:18 +0300

    Removing OnInit stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { PostComponent } from './post/post.component'
 
 @Component({
@@ -6,14 +6,13 @@ import { PostComponent } from './post/post.component'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
   title = 'AngIntro';
   parentMessage: string = "Parent Message";
   
   @ViewChild(PostComponent) childComponent !: PostComponent;
 
-  //constructor() {
-  ngOnInit() {
-    console.log(this.childComponent.childMessage);
+  constructor() {
+    console.log(this.childComponent);
   }
 }
