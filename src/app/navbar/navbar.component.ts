9d34d235f1e91461338684452a28676f9a0d10cc
Dadaskis <dadaskis@gmail.com>
2023-07-05 14:40:26 +0300

    Moving HTML/CSS code to external files

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,17 +2,9 @@ import {Component} from '@angular/core';
 
 @Component({
 	selector: 'app-navbar',
-	template: `
-		<h1>Navbar Component</h1>
-		<p>Any text over here</p>
-	`,
-	styles: [
-		`
-		h1 { 
-			color: blue;
-			font-size: 50px;
-		}
-		`
+	templateUrl: './navbar.component.html',
+	styleUrls: [
+		'./navbar.component.css'
 	]
 })
 export class NavbarComponent {
