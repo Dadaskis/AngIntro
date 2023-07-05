41d9d4bd9199a110da7fd105614442df1e0bb78a
Dadaskis <dadaskis@gmail.com>
2023-07-05 14:33:25 +0300

    Multi-line app-navbar template text, adding <p> tag

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,10 @@ import {Component} from '@angular/core';
 
 @Component({
 	selector: 'app-navbar',
-	template: '<h1>Navbar Component</h1>',
+	template: `
+		<h1>Navbar Component</h1>
+		<p>Any text over here</p>
+	`,
 	styles: [
 		`
 		h1 { 
