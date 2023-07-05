de65baa38c67f0c64c1b36e1dacbbec19a0b7623
Dadaskis <dadaskis@gmail.com>
2023-07-05 15:16:10 +0300

    Removing OnInit from post component

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent implements OnInit {
+export class PostComponent {
 
   title: string = "List of Posts";
-
-  constructor() {}
-
-  ngOnInit() : void {
-
-  }
 
 }
