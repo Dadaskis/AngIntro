45507e27db40eebf83b535c2994d8b62c3c6254d
Dadaskis <dadaskis@gmail.com>
2023-07-05 14:52:59 +0300

    Removing test components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostComponent } from './post/post.component';
-import { TestComponent } from './test/test.component';
-import { Test2Component } from './test2/test2.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    PostComponent,
-    TestComponent,
-    Test2Component
+    PostComponent
   ],
   imports: [
     BrowserModule,
