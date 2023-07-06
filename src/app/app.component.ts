2a379b5b0813f0c703f063edbba042b3a504f01c
Dadaskis <dadaskis@gmail.com>
2023-07-06 15:36:10 +0300

    Attempt to make logic (untested)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+export interface UserData {
+  address: string;
+  email: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
+  addressValue: string;
+  emailValue: string;
+  usernameValue: string;
+
+  userDataBase: Array<UserData> = []
+
+  saveData() {
+    data = {
+      address: addressValue,
+      email: emailValue,
+      name: usernameValue
+    }
+    userDataBase.push(data)
+
+    addressValue = "";
+    emailValue = "";
+    usernameValue = "";
+  }
+
+  removeUser(index) {
+    this.userDataBase.splice(index, 1)
+  }
 }
