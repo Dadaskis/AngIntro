e422348f4d723d11dd5a9b0a2f70c5bcc1c5d1cc
Dadaskis <dadaskis@gmail.com>
2023-07-06 12:20:03 +0300

    Trying to fetch object array on my own

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,13 @@ export class AppComponent {
   postArray: Array<string> = [
     'Post 1', 'Post 2', 'Post 3', 'Post 4', 'Post 5'
   ]
+
+  objArray: Array<Object> = [
+    { id: 0, postTitle: "Where to go?" },
+    { id: 1, postTitle: "Post 1." },
+    { id: 3, postTitle: "Front-End 101" },
+    { id: 4, postTitle: "Hey, you!" },
+    { id: 5, postTitle: "Post 5." },
+    { id: 8, postTitle: "Post 10." },
+  ]
 }
