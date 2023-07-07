4f333d9b07301c7c76d72919339a9c4c0b5dde49
Dadaskis <dadaskis@gmail.com>
2023-07-08 01:50:55 +0300

    Trying to add logic on submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms'
 
+import { UsersService } from 'services/users.service'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  usersService?: UsersService
+
+  constructor(private service: UsersService) {
+    this.usersService = service
+  }
+
   onSubmit(fillForm: NgForm) {
-    
+    if(!fillForm.valid) {
+      return
+    }
+
+    usersService.addUser({
+      uniqueID: 0,
+      name: fillForm.values['fullName'],
+      displayName: fillForm.values['fullName']
+    })
   }
 }
