cb8b5314ace325c2168c0bddd6aa63cb97b15746
Dadaskis <dadaskis@gmail.com>
2023-07-07 17:23:21 +0300

    Removing append-cli

--- src/app/append-cli.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppendCliPipe } from './append-cli.pipe';
-
-describe('AppendCliPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AppendCliPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
