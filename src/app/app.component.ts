be9f54678553a49e3d44cd685ea5890d330dfcf8
Dadaskis <dadaskis@gmail.com>
2023-07-07 16:26:20 +0300

    Removing contents of current app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
 
-export interface UserData {
-  address: string;
-  email: string;
-  name: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,26 +7,4 @@ export interface UserData {
 })
 export class AppComponent {
 
-  usernameValue: string = "";
-  emailValue: string = "";
-  addressValue: string = "";
-
-  userDataBase: Array<UserData> = []
-
-  saveData() {
-    let data: UserData = {
-      address: this.addressValue,
-      email: this.emailValue,
-      name: this.usernameValue
-    }
-    this.userDataBase.push(data)
-
-    this.addressValue = "";
-    this.emailValue = "";
-    this.usernameValue = "";
-  }
-
-  removeUser(index: any) {
-    this.userDataBase.splice(index, 1)
-  }
 }
