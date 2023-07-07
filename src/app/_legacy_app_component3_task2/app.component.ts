3fb443db18661517a1fa50008ad5c65e7bdea1f8
Dadaskis <dadaskis@gmail.com>
2023-07-07 16:25:21 +0300

    Moving current app.component to legacy folder

---
+++ src/app/_legacy_app_component3_task2/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+export interface UserData {
+  address: string;
+  email: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  usernameValue: string = "";
+  emailValue: string = "";
+  addressValue: string = "";
+
+  userDataBase: Array<UserData> = []
+
+  saveData() {
+    let data: UserData = {
+      address: this.addressValue,
+      email: this.emailValue,
+      name: this.usernameValue
+    }
+    this.userDataBase.push(data)
+
+    this.addressValue = "";
+    this.emailValue = "";
+    this.usernameValue = "";
+  }
+
+  removeUser(index: any) {
+    this.userDataBase.splice(index, 1)
+  }
+}
---
+++ src/app/_legacy_app_component3_task2/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { FormFillComponent } from './form-fill/form-fill.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    PostComponent,
+    PostListComponent,
+    FormFillComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
